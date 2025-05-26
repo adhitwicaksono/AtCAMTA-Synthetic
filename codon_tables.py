07d40bf260333de75b2a924b5b510b834327f94e
Adhityo Wicaksono <107833553+adhitwicaksono@users.noreply.github.com>
2025-05-26 13:24:46 +0700

    Create codon_tables.py

---
+++ codon_tables.py
@@ -0,0 +1,30 @@
+# codon_tables.py
+# Reversible codon tables for translation and back-translation
+
+# Amino acid to codons (reverse table)
+reverse_codon_table = {
+    'A': ['GCT', 'GCC', 'GCA', 'GCG'],
+    'C': ['TGT', 'TGC'],
+    'D': ['GAT', 'GAC'],
+    'E': ['GAA', 'GAG'],
+    'F': ['TTT', 'TTC'],
+    'G': ['GGT', 'GGC', 'GGA', 'GGG'],
+    'H': ['CAT', 'CAC'],
+    'I': ['ATT', 'ATC', 'ATA'],
+    'K': ['AAA', 'AAG'],
+    'L': ['TTA', 'TTG', 'CTT', 'CTC', 'CTA', 'CTG'],
+    'M': ['ATG'],
+    'N': ['AAT', 'AAC'],
+    'P': ['CCT', 'CCC', 'CCA', 'CCG'],
+    'Q': ['CAA', 'CAG'],
+    'R': ['CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],
+    'S': ['TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'],
+    'T': ['ACT', 'ACC', 'ACA', 'ACG'],
+    'V': ['GTT', 'GTC', 'GTA', 'GTG'],
+    'W': ['TGG'],
+    'Y': ['TAT', 'TAC'],
+    '*': ['TAA', 'TAG', 'TGA']
+}
+
+# Codon to amino acid (forward table)
+codon_table = {codon: aa for aa, codons in reverse_codon_table.items() for codon in codons}
